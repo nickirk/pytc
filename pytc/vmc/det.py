4c94aba4220a3af401172780d3e34efb02f7ee61
keliao <ke.liao.whu@gmail.com>
2025-02-03 14:11:39 +0100

    update det and test_det

--- pytc/vmc/det.py
+++ pytc/vmc/det.py
@@ -2,16 +2,20 @@ import numpy as np
 from pyscf.dft import numint
 
 class SlaterDeterminant:
-    def __init__(self, mol, mo_coeff=None):
+    def __init__(self, mol, mo_coeff=None, nelec=None):
         """
         Args:
         mol: A PySCF mol object (provides integrals, eval_gto, etc.)
         mo_coeff: Either a single np.ndarray of shape (nAOs, nMOs) for RHF,
         or a tuple/list [mo_coeff_alpha, mo_coeff_beta] each of
         shape (nAOs, nMOs) for UHF.
+        nelec: Number of electrons as a tuple (n_alpha, n_beta).
         """
         self.mol = mol
-        self.n_up, self.n_down = mol.nelec 
+        if nelec is None:
+            self.n_alpha, self.n_beta = mol.nelec 
+        else:
+            self.n_alpha, self.n_beta = nelec
     
         # Detect if mo_coeff is restricted or unrestricted:
         if isinstance(mo_coeff, (list, tuple)):
@@ -34,6 +38,16 @@ class SlaterDeterminant:
         self.det_down = None
         self.last_positions = None
     
+    def __call__(self, coords):
+        """
+        Convenience method to call value on a set of coordinates.
+    
+        Args:
+            coords: (n_up + n_down, 3) electron positions
+        Returns:
+            float: The product of alpha and beta determinants
+        """
+        return self.value(coords)
 
     def value_and_grad(self, coords):
         """Compute both value and gradient of Slater determinant.
@@ -60,21 +74,21 @@ class SlaterDeterminant:
                 Each element (i,j,k) represents ∇ᵢϕⱼ(rᵢ) in direction k
         """
         # Get AO values and gradients using numint (returns tuple (value, grad))
-        ao_vals, ao_grads = numint.eval_ao(self.mol, coords, deriv=1)
+        ao_grads = numint.eval_ao(self.mol, coords, deriv=1)[1:].transpose(1, 2, 0)
         
         # Split into up and down electron parts
-        ao_grads_up = ao_grads[:self.n_up]      # shape (n_up, nAOs, 3)
-        ao_grads_down = ao_grads[self.n_up:]    # shape (n_down, nAOs, 3)
+        ao_grads_up = ao_grads[:self.n_alpha]      # shape (n_up, nAOs, 3)
+        ao_grads_down = ao_grads[self.n_alpha:]    # shape (n_down, nAOs, 3)
         
         # Contract with MO coefficients to get gradients of molecular orbitals
         # For each spatial direction, multiply ao_grad by mo_coeff
-        grad_up = np.zeros((self.n_up, self.n_up, 3))
-        grad_down = np.zeros((self.n_down, self.n_down, 3))
+        grad_up = np.zeros((self.n_alpha, self.n_alpha, 3))
+        grad_down = np.zeros((self.n_beta, self.n_beta, 3))
         
         # Handle each spatial direction
         for d in range(3):
-            grad_up[..., d] = ao_grads_up[..., d] @ self.mo_coeff_alpha[:, :self.n_up]
-            grad_down[..., d] = ao_grads_down[..., d] @ self.mo_coeff_beta[:, :self.n_down]
+            grad_up[..., d] = ao_grads_up[..., d] @ self.mo_coeff_alpha[:, :self.n_alpha]
+            grad_down[..., d] = ao_grads_down[..., d] @ self.mo_coeff_beta[:, :self.n_beta]
             
         return grad_up, grad_down
     
@@ -91,17 +105,16 @@ class SlaterDeterminant:
                 Each element (i,j) represents ∇²ᵢϕⱼ(rᵢ)
         """
         # Get AO values, gradients, and laplacians using numint
-        ao_data = numint.eval_ao(self.mol, coords, deriv=2)
-        ao_vals, ao_grads, ao_lapls = ao_data
+        ao_lapls = numint.eval_ao(self.mol, coords, deriv=2)[4:].transpose(1, 2, 0)
         
         # Split laplacians into up and down electron parts
         # Note: PySCF returns laplacian as sum of second derivatives
-        ao_lapls_up = ao_lapls[:self.n_up]      # shape (n_up, nAOs)
-        ao_lapls_down = ao_lapls[self.n_up:]    # shape (n_down, nAOs)
+        ao_lapls_up = ao_lapls[:self.n_alpha]      # shape (n_up, nAOs)
+        ao_lapls_down = ao_lapls[self.n_alpha:]    # shape (n_down, nAOs)
         
         # Contract with MO coefficients to get laplacians of molecular orbitals
-        lapl_up = ao_lapls_up @ self.mo_coeff_alpha[:, :self.n_up]    # shape (n_up, n_up)
-        lapl_down = ao_lapls_down @ self.mo_coeff_beta[:, :self.n_down]  # shape (n_down, n_down)
+        lapl_up = ao_lapls_up @ self.mo_coeff_alpha[:, :self.n_alpha]    # shape (n_up, n_up)
+        lapl_down = ao_lapls_down @ self.mo_coeff_beta[:, :self.n_beta]  # shape (n_down, n_down)
             
         return lapl_up, lapl_down
     
@@ -117,19 +130,19 @@ class SlaterDeterminant:
             slater_up, slater_down: the Slater matrices for alpha and beta spins
         """
         # Get AO values using numint
-        ao_vals_all = numint.eval_ao(coords, deriv=0)
+        ao_vals_all = numint.eval_ao(self.mol, coords, deriv=0)
     
         # Partition the AO values by spin
-        ao_up = ao_vals_all[:self.n_up]      # shape (n_up, nAOs)
-        ao_down = ao_vals_all[self.n_up:]    # shape (n_down, nAOs)
+        ao_up = ao_vals_all[:self.n_alpha]      # shape (n_up, nAOs)
+        ao_down = ao_vals_all[self.n_alpha:]    # shape (n_down, nAOs)
     
         # Multiply AO by mo_coeff
         mat_up = ao_up @ self.mo_coeff_alpha     # shape (n_up, nMOs_alpha)
         mat_down = ao_down @ self.mo_coeff_beta  # shape (n_down, nMOs_beta)
     
         # Select only the orbitals up to number of electrons for each spin
-        slater_up = mat_up[:, :self.n_up]        # shape (n_up, n_up)
-        slater_down = mat_down[:, :self.n_down]  # shape (n_down, n_down)
+        slater_up = mat_up[:, :self.n_alpha]        # shape (n_up, n_up)
+        slater_down = mat_down[:, :self.n_beta]  # shape (n_down, n_down)
     
         return slater_up, slater_down
     
@@ -142,10 +155,11 @@ class SlaterDeterminant:
         Returns:
             float: The product of alpha and beta determinants
         """
-        slater_up, slater_down = self.compute_slater_matrices(coords)
+        slater_up, slater_down = self.matrix(coords)
         det_up = np.linalg.det(slater_up)
         det_down = np.linalg.det(slater_down)
         return det_up * det_down
+    
     
     def init_inverse(self, coords):
         """
@@ -155,7 +169,7 @@ class SlaterDeterminant:
         Args:
             coords: (n_up + n_down, 3)
         """
-        slater_up, slater_down = self.compute_slater_matrices(coords)
+        slater_up, slater_down = self.matrix(coords)
         self.inv_up = np.linalg.inv(slater_up)
         self.inv_down = np.linalg.inv(slater_down)
         self.det_up = np.linalg.det(slater_up)
@@ -174,23 +188,23 @@ class SlaterDeterminant:
             ratio: float, ratio of new determinant to old determinant for that spin block
         """
         # Figure out if alpha or beta
-        is_alpha = (e_idx < self.n_up)
+        is_alpha = (e_idx < self.n_alpha)
         spin_inv = self.inv_up if is_alpha else self.inv_down
         old_det = self.det_up if is_alpha else self.det_down
     
         # Evaluate AO for the new position
-        ao_new = self._eval_AO(new_pos.reshape(1, 3)).squeeze(axis=0)
+        ao_new = numint.eval_ao(self.mol, new_pos.reshape(1, 3)).squeeze(axis=0)
     
         # Build new row. If alpha spin, use mo_coeff_alpha, else mo_coeff_beta
         mo_coeff_spin = self.mo_coeff_alpha if is_alpha else self.mo_coeff_beta
-        n_spin = self.n_up if is_alpha else self.n_down
+        n_spin = self.n_alpha if is_alpha else self.n_beta
     
         # Build the row (shape (n_spin,))
         new_row = ao_new @ mo_coeff_spin
         new_row = new_row[:n_spin]
     
         # local index within that spin
-        local_idx = e_idx if is_alpha else e_idx - self.n_up
+        local_idx = e_idx if is_alpha else e_idx - self.n_alpha
     
         # ratio = new_row dot (spin_inv column local_idx)
         ratio = new_row @ spin_inv[:, local_idx]
