4c2588fe09bda7218a6db757397567a4938b7ee6
Ke Liao <nickirk@users.noreply.github.com>
2025-02-15 11:09:57 +0100

    Update test_fcidump.py
    Change accordingly the test file

--- pytc/utils/test/test_fcidump.py
+++ pytc/utils/test/test_fcidump.py
@@ -2,7 +2,7 @@ import unittest
 import os
 import numpy as np
 from pyscf import gto, scf
-from pytc.utils.fcidump import write_fcidump, read_fcidump
+from pytc.utils import fcidump
 from pytc.xtc import XTC
 from pytc.jastrow import SimpleJastrow
 
@@ -28,8 +28,8 @@ class TestFCIDump(unittest.TestCase):
         n_orb_xtc = h1e_xtc.shape[0]
         n_elec_xtc = self.mol.nelec[0]
         
-        write_fcidump(self.filename, h1e_xtc, h2e_xtc, ecore_xtc, n_orb_xtc, n_elec_xtc)
-        n_orb_fcidump, n_elec_fcidump, ecore_fcidump, h1e_fcidump, h2e_fcidump = read_fcidump(self.filename)
+        fcidump.write(self.filename, h1e_xtc, h2e_xtc, ecore_xtc, n_orb_xtc, n_elec_xtc)
+        n_orb_fcidump, n_elec_fcidump, ecore_fcidump, h1e_fcidump, h2e_fcidump = fcidump.read(self.filename)
         
         self.assertEqual(n_orb_xtc, n_orb_fcidump)
         self.assertEqual(n_elec_xtc, n_elec_fcidump)
