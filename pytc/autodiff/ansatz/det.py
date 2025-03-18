e9760edc7ad3b244686b76a6769357f115aa8b83
Ke Liao <ke.liao.whu@gmail.com>
2025-03-18 12:46:23 +0100

    Fix to the previous fix for shapes related to matrix and grad in det.py

--- pytc/autodiff/ansatz/det.py
+++ pytc/autodiff/ansatz/det.py
@@ -157,7 +157,7 @@ class SlaterDet:
                 grad_batch[..., d] = np.dot(ao_grads[..., d], self.mo_coeff_alpha_occ).reshape(n_walkers, self.n_electrons, -1)
             
             grad_up_batch = grad_batch[:, :self.n_alpha, :self.n_alpha]
-            grad_down_batch = grad_batch[:, self.n_alpha:, self.n_alpha:]
+            grad_down_batch = grad_batch[:, self.n_alpha:, :self.n_beta]
             
         else:
             # Reshape back to batch form
@@ -265,7 +265,7 @@ class SlaterDet:
             slater_batch = np.dot(ao_vals_all, self.mo_coeff_alpha_occ).reshape(n_walkers, n_electrons, -1)
 
             slater_up_batch = slater_batch[:,:self.n_alpha, :self.n_alpha]
-            slater_down_batch = slater_batch[:, self.n_alpha:, self.n_alpha:]
+            slater_down_batch = slater_batch[:, self.n_alpha:, :self.n_beta]
 
         else:
             # Split AO values by spin - shape: (n_walkers, n_up, nAOs) and (n_walkers, n_down, nAOs)
