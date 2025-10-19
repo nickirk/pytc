f459eba673a98e249412e50f28fa9237161954df
keliao <ke.liao.whu@gmail.com>
2025-10-19 11:55:01 -0400

    - add benzene as test example, for performance profiling. - update profile.py - change default sampling to non-importance sampling

--- pytc/utils/profile.py
+++ pytc/utils/profile.py
@@ -1,8 +1,12 @@
+# Add at the top of your script, BEFORE importing JAX
+import os
+os.environ['XLA_FLAGS'] = '--xla_cpu_multi_thread_eigen=true intra_op_parallelism_threads=8'
 from line_profiler import LineProfiler
 import unittest
 import io
 from pytc.autodiff.test.test_mcmc import TestHartreeFockEnergy
-from pytc.autodiff.mcmc import metropolis_hastings
+from pytc.autodiff.mcmc import metropolis_hastings, sample, metropolis_hastings_importance_sampling
+from pytc.autodiff.mcmc import burn_in_with_importance, metropolis_hastings, _one_electron_move
 from pytc.autodiff.ansatz.det import SlaterDet
 from pytc.autodiff.ansatz.sj import SlaterJastrow
 from pyscf.dft import numint  # Import numint to profile AO evaluations
@@ -16,19 +20,25 @@ profile = LineProfiler()
 # Add functions to profile
 profile.add_function(test.run_hf_energy_test)
 profile.add_function(metropolis_hastings)
+profile.add_function(sample)
+profile.add_function(metropolis_hastings)
+profile.add_function(_one_electron_move)
+
 
 # Add SlaterDet functions that are likely bottlenecks
+profile.add_function(SlaterDet.__call__)
 profile.add_function(SlaterDet.value)
 profile.add_function(SlaterDet.matrix)
 profile.add_function(SlaterDet.grad)
 profile.add_function(SlaterDet.laplacian)
+profile.add_function(SlaterDet.ao2mo)
 profile.add_function(SlaterJastrow.local_energy)
 
 # Add the PySCF numint.eval_ao function which is the deepest bottleneck
 profile.add_function(numint.eval_ao)
 
 # Run the profiled test
-profile.runcall(test.test_he_atom)
+profile.runcall(test.test_benzene)
 
 # Print results to console
 profile.print_stats()
